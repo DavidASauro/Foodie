aba9f86c36c3ba1423f451ee642426148a11e5f2
DavidASauro <DavidSau43@gmail.com>
2025-08-29 19:55:02 -0400

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,7 @@ func main() {
     r := gin.Default()
 
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"https://foodie.pages.dev"}, // frontend URL
+        AllowOrigins:     []string{"https://foooodie.pages.dev"}, // frontend URL
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
         AllowCredentials: true,
